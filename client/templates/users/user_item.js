6270e197083db133545a9fe0a5ddd1e897158286
mmedarhri <m.medarhri@gmail.com>
2014-11-20 02:29:46 +0100

    update

--- client/templates/users/user_item.js
+++ client/templates/users/user_item.js
@@ -1,10 +1,7 @@
 Template.userItem.helpers({
   name: function() {
     return _.extend({}, simo_users.findOne({"username": Meteor.user().username}), this).username;
-      },
-  mail: function() {
-    return _.extend({}, simo_users.find({"username": "Mohamed"},{'emails.address':1,'_id':0}),this);
-  }
+      }
 /*
   address: function() {
     return _.extend({}, simo_users.findOne({"username": Meteor.user().username}), this).address;
